2c6c666193dcf8be8a3dc0958ea0e51d67bbff66
Damien Butt <damien.butt@norgateav.com>
2022-05-08 00:30:24 +0100

    feat(storybook): add global argTypes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import { ColorControl } from "@storybook/components";
+
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
     // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
@@ -10,6 +12,24 @@ export const parameters = {
 };
 
 export const argTypes = {
-    fontSize: { type: "number", defaultValue: 100 },
-    color: { type: "string", defaultValue: "#000" },
+    theme: { control: { options: ["light", "dark"] } },
+    fontSize: {
+        name: "Font Size",
+        type: { name: "number", required: true },
+        defaultValue: 100,
+        description: "Font size in pixels",
+        control: {
+            type: null,
+        },
+    },
+    color: {
+        name: "Color",
+        type: { name: "string", required: true },
+        defaultValue: "#000",
+        description: "Color of the text",
+        control: {
+            type: null,
+        },
+    },
 };
+export const args = { theme: "dark" };
