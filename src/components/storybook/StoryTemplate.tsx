c215dbbcbdae893bfcfd2adccef38eabf070046f
Damien Butt <damien.butt@norgateav.com>
2022-05-08 00:31:43 +0100

    feat(storybook): add generic story template

--- src/components/storybook/StoryTemplate.tsx
+++ src/components/storybook/StoryTemplate.tsx
@@ -1,21 +1,23 @@
 import React from "react";
-// import { Story } from "@storybook/react";
-import { BaseComponentType /* ICustomProps */ } from "../../types";
+import { Story } from "@storybook/react";
+import { BaseComponentType } from "../../types";
 
-export const StoryTemplate = <T extends React.JSXElementConstructor<T>>(
-    { fontSize, color, ...args }: BaseComponentType<T>,
-    { component: Component }: any,
-): any => {
-    const style = {
-        fontSize: `${fontSize}px`,
-        color,
+export const StoryTemplate =
+    <T extends React.JSXElementConstructor<T>>(
+        { fontSize, color }: Partial<BaseComponentType<T>>,
+        { component: Component }: any,
+    ): Story<BaseComponentType<T>> =>
+    (args) => {
+        const style = {
+            fontSize: `${fontSize}px`,
+            color,
+        };
+
+        return (
+            <div style={style}>
+                <Component {...args} />
+            </div>
+        );
     };
-
-    return (
-        <div style={style}>
-            <Component {...args} />
-        </div>
-    );
-};
 
 export default StoryTemplate;
