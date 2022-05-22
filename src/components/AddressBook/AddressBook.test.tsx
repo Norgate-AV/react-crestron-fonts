4b2d37fa5d28181930b47dddf7e8826305ca52da
Damien Butt <damien.butt@norgateav.com>
2022-05-22 18:24:05 +0100

    test: use @testing-library/react to render component

--- src/components/AddressBook/AddressBook.test.tsx
+++ src/components/AddressBook/AddressBook.test.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "@testing-library/react";
 import { AddressBook } from "./AddressBook";
+import "@testing-library/jest-dom";
 
 describe("AddressBook", () => {
     it("renders without crashing", () => {
-        const div = document.createElement("div");
-        ReactDOM.render(<AddressBook />, div);
-        ReactDOM.unmountComponentAtNode(div);
+        render(<AddressBook />);
     });
 });
