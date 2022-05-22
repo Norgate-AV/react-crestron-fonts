0f6b73dc377e3a75229db5a563be42492ada50f8
Damien Butt <damien.butt@norgateav.com>
2022-05-22 02:38:31 +0100

    refactor(storybook): remove argTypes and defaultArgs files

--- src/components/storybook/argTypes.ts
+++
@@ -1,6 +0,0 @@
-export const argTypes = {
-    fontSize: { type: "number", defaultValue: 100 },
-    color: { type: "string", defaultValue: "#000" },
-};
-
-export default argTypes;
--- src/components/storybook/defaultArgs.ts
+++
@@ -1,5 +0,0 @@
-export const defaultArgs = {
-    fontSize: 100,
-};
-
-export default defaultArgs;
