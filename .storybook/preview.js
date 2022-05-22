2894b7c36fe90f0f5cec506bc993d256fc1bd36d
Damien Butt <damien.butt@norgateav.com>
2022-05-22 02:40:41 +0100

    refactor(storybook): only set label for color global argType

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -12,25 +12,16 @@ export const parameters = {
 };
 
 export const argTypes = {
-    theme: { control: { options: ["light", "dark"] } },
     fontSize: {
         name: "Font Size",
-        type: { name: "number", required: true },
-        defaultValue: 100,
+        type: { name: "number", required: false },
+        defaultValue: 150,
         description: "Font size in pixels",
         control: {
-            type: null,
+            type: "number",
         },
     },
     color: {
         name: "Color",
-        type: { name: "string", required: true },
-        defaultValue: "#000",
-        description: "Color of the text",
-        control: {
-            type: null,
-        },
     },
 };
-
-export const args = { theme: "dark" };
