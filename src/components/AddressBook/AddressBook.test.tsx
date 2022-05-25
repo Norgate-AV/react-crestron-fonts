9c128d5cdaa6a895d6871c2fc4c3446c3cd005d6
Damien Butt <damien.butt@norgateav.com>
2022-05-26 00:08:09 +0100

    test: add snapshot tests

--- src/components/AddressBook/AddressBook.test.tsx
+++ src/components/AddressBook/AddressBook.test.tsx
@@ -1,10 +1,40 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import { Black as AddressBook } from "./AddressBook.stories";
+import { Black, White, Custom } from "./AddressBook.stories";
+import { AddressBook } from "..";
 import "@testing-library/jest-dom";
 
-describe("AddressBook", () => {
+const component = AddressBook;
+const stories = {
+    black: `${component.name}Story/Black`,
+    white: `${component.name}Story/White`,
+    custom: `${component.name}Story/Custom`,
+};
+
+describe(`${component.name}`, () => {
     it("renders without crashing", () => {
-        render(<AddressBook fontSize={150} color="#000" />);
+        const tree = render(<AddressBook />);
+        expect(tree).toMatchSnapshot();
+    });
+});
+
+describe(`${stories.black}`, () => {
+    it("renders without crashing", () => {
+        const tree = render(<Black />);
+        expect(tree).toMatchSnapshot();
+    });
+});
+
+describe(`${stories.white}`, () => {
+    it("renders without crashing", () => {
+        const tree = render(<White />);
+        expect(tree).toMatchSnapshot();
+    });
+});
+
+describe(`${stories.custom}`, () => {
+    it("renders without crashing", () => {
+        const tree = render(<Custom />);
+        expect(tree).toMatchSnapshot();
     });
 });
